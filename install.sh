1f3d2221d8a5a9729bf86a924f75582e804938b9
Juan Viola <juanviola@gmail.com>
2016-11-21 01:27:03 +0000

    fixing playbooks

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y git 
+apt-get update && apt-get install -y git python-pip libyaml-dev libpython2.7-dev
 pip install --upgrade ansible
+pip install markupsafe
+mkdir -p /etc/ansible && echo 'localhost     ansible_connection=local' > /etc/ansible/hosts
