9349ba9a4e9920774294df7f70f9707da858d4f4
Juan Viola <juanviola@gmail.com>
2017-01-23 22:38:28 -0300

    adding library libffi-dev to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y git python-pip libyaml-dev libpython2.7-dev
+apt-get update && apt-get install -y git python-pip libyaml-dev libpython2.7-dev libffi-dev
 pip install --upgrade ansible
 pip install markupsafe
 mkdir -p /etc/ansible && echo 'localhost     ansible_connection=local' > /etc/ansible/hosts
