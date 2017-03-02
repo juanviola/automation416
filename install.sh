c17b24567d14af1b78192a3f28c3200816abdeac
Juan Viola <juan.viola@mercadolibre.com>
2017-03-01 21:53:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y git libyaml-dev libpython2.7-dev libffi-dev
+apt-get update && apt-get install -y git libyaml-dev libpython2.7-dev libffi-dev libssl-dev 
 curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py && chmod +x /tmp/get-pip.py && /tmp/get-pip.py
 /usr/local/bin/pip install --upgrade ansible
 /usr/local/bin/pip install markupsafe
