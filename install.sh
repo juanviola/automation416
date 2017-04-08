83f2dfd3bb0291fb1ef0ffcec54c3f6a2a5cb175
Juan Viola <juan.viola@mercadolibre.com>
2017-04-08 18:43:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y git libyaml-dev libpython2.7-dev libffi-dev libssl-dev lsof vim python-setuptools python-pip
-#curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py && chmod +x /tmp/get-pip.py && /tmp/get-pip.py
+apt-get update && apt-get install -y git libyaml-dev libpython2.7-dev libffi-dev libssl-dev lsof vim python-setuptools 
+curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py && chmod +x /tmp/get-pip.py && /tmp/get-pip.py
 /usr/local/bin/pip install --upgrade ansible
 /usr/local/bin/pip install markupsafe
 mkdir -p /etc/ansible && echo 'localhost     ansible_connection=local' > /etc/ansible/hosts
